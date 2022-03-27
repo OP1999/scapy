02d4d4b61469d34d2ffeb11dfd28e8e18a2b7cd6
40235605 <oranpower@hotmail.co.uk>
2022-03-27 19:07:02 +0100

    Added GUI to inherit from

--- Client.py
+++ Client.py
@@ -1,6 +1,7 @@
 from scapy.all import *
 import socket
 import NTPMethods
+import GUI
 import PySimpleGUI as sg
 
 localIP = NTPMethods.clientIP
@@ -19,38 +20,10 @@ global ntpMode
 global ntpMessage
 global response
 
-# ----------- Create the layouts this Window will display -----------
-layoutReceive = [   [sg.Text("Client in NTP Receive Mode")]     ]
-
-layoutResponse = [   [sg.Text(key="-Mess-")],
-                        [sg.Text(key="-NTPText-")]    ]
-
-layoutButton = [    [sg.Button('Message'), sg.Button('Text File'), sg.Button('Image File'), sg.Button('Zip File')]    ]
-
-layoutMes = [ [sg.Text("Input the message you would like to send to the server")],
-            [sg.Input(key="-INMes-")]    ]
-
-layoutTxt = [ [sg.Text('Send Text File via NTP')],
-            [sg.Text("Choose a file: "), sg.Input(key="-INTxt-", change_submits=True), sg.FileBrowse(key="-INFBTxt-", file_types=(("Text Files", "*.txt")))] ]
-            
-layoutImg = [ [sg.Text('Send Image via NTP')],
-            [sg.Text("Choose a file: "), sg.Input(key="-INImg-", change_submits=True), sg.FileBrowse(key="-INFBImage-", file_types=(("Image Files", "*.jpg")))] ]
-
-layoutZip = [ [sg.Text('Send Zip via NTP')],
-            [sg.Text("Choose a file: "), sg.Input(key="-INZip-", change_submits=True), sg.FileBrowse(key="-INFBZip-", file_types=(("Zip Files", "*.zip")))] ]
-
-# ----------- Create actual layout using Columns and a row of Buttons
-winLayout = [   [sg.Text('Client')],
-            [sg.Button('Receive Mode'), sg.Button('Send Mode')],
-            [sg.Column(layoutReceive, key='-COLReceive-', visible=False)],
-            [sg.Column(layoutButton, key='-COLBtn-', visible=False)],
-            [sg.Column(layoutMes, visible=False, key='-COLMes-'), sg.Column(layoutTxt, visible=False, key='-COLTxt-'), sg.Column(layoutImg, visible=False, key='-COLImg-'), sg.Column(layoutZip, visible=False, key='-COLZip-')],
-            [sg.Column(layoutResponse, key='-COLResponse-', visible=False)],
-            [sg.Button('Send', key='-BTNSend-', visible=False)], 
-            [sg.Button('Exit', key='-BTNExit-')]    ]
-window = sg.Window('Send / Receive Data via NTP', winLayout, size=(500,300), element_justification='c')
+window = sg.Window('Client', GUI.winLayout, size=(500,300), element_justification='c')
 
 layout = 1  # The currently visible layout
+
 while True:
     event, values = window.read()
     if event in (None, '-BTNExit-'):
